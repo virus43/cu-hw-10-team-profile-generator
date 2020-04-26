6e1c297fc0d61f7fad47b42acfb2c3a18452516f
Viraj Shetty <viraj.vls@gmail.com>
2020-04-26 15:02:15 -0400

    first commit

--- app.js
+++ app.js
@@ -13,10 +13,11 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
  
+let team = []
+
 if (require.main === module) { 
     addMember(); 
 }
-
 
 function addMember() {
     inquirer
@@ -44,15 +45,16 @@ function addMember() {
 
             switch(answers.role) {
                 case "Manager":
-                    info="officer number";
+                    info = "office number";
                     break;
                 case "Engineer":
-                    info="github username";
+                    info = "github username";
                     break;
                 case "Intern":
-                    info="school name";
+                    info = "school name";
                     break;
             };
+
             inquirer
                 .prompt([
                     {
@@ -67,34 +69,36 @@ function addMember() {
                     }
 
                 ])
-                .then(answers => {
-                    if(answers.addMore ==='Yes') {
+                .then(data => {
+                    switch(answers.role) {
+                        case "Manager":
+                            obj = new Manager(answers.name,answers.id,answers.email,data.additionalInfo);
+                        
+                            break;
+                        case "Engineer":
+                            info = "github username";
+                            obj = new Engineer(answers.name,answers.id,answers.email,data.additionalInfo);
+                            break;
+                        case "Intern":
+                            info = "school name";
+                            obj = new Intern(answers.name,answers.id,data.additionalInfo);
+                            break;
+                    }
+
+                    team.push(obj);
+                    fs.writeFile(outputPath, render(team), function(err) {
+                        if (err) {
+                            console.log(err);
+                        }
+                    });
+                    if(data.addMore ==='Yes') {
                         addMember();
                     }
                     else {
                         process.exit(1);
-                    }
+                        };
+
                 })
 
         })
 }
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
